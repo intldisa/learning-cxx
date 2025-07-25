d49a82eadd4b69380c3ac06080876164f0e26d58
BB265 <2655732084@qq.com>
2025-07-26 00:05:20 +0800

    0 - 17 completed

--- exercises/15_class_clone/main.cpp
+++ exercises/15_class_clone/main.cpp
@@ -10,17 +10,26 @@ class DynFibonacci {
 
 public:
     // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new ?), cached(?) {}
+    DynFibonacci(int capacity): cache(new size_t[capacity]{0, 1, 1}), cached(2) {
+        for (; cached <= capacity; ++cached) {
+            cache[cached] = cache[cached - 1] + cache[cached - 2];
+        }
+    }
 
     // TODO: 实现复制构造器
-    DynFibonacci(DynFibonacci const &) = delete;
+    DynFibonacci(DynFibonacci const & fib_): cache(new size_t[fib_.cached]), cached(fib_.cached) {
+        std::copy(fib_.cache, fib_.cache + fib_.cached, cache);
+        // for(int i = 0; i <= cached; ++i) cache[i] = fib_.cache[i];
+    }
 
     // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci();
+    ~DynFibonacci() {
+        delete[] cache;
+    }
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (; false; ++cached) {
+        for (; cached <= i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
